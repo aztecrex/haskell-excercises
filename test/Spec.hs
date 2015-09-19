3d74916c24f88fc9010e04abea29bf070ae4a102
Greg Wiley <aztec.rex@jammm.com>
2015-09-19 12:02:57 -0700

    add furry tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
+import Excercises
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn ""
+  putStrLn "- TESTS -----------------------------------"
+  report "furryForList" $ assertEqual [2,4,6] $ furry (*2) [1,2,3]
+  report "furryforEmptyList" $  assertNull $ furry (*2) []
+  putStrLn "-------------------------------------------"
+
+data Test = OK | WahWah String
+
+report :: String -> Test -> IO ()
+report name OK = putStrLn $ "'" ++ name ++ "' is OK!"
+report name (WahWah message) = putStrLn $ "'" ++ name ++
+        "' failed (" ++ message ++ ")."
+
+assertEqual :: (Eq a, Show a) => a -> a -> Test
+assertEqual expected actual = if expected == actual
+  then OK
+  else WahWah $ "not equal: " ++ show (expected,actual)
+
+assertNull :: (Show a) => [a] -> Test
+assertNull xs = if null xs
+  then OK
+  else WahWah $ "not null:" ++ show xs
